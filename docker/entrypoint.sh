9da9863357147734b9e4a10a9d7f5a4bfe05b6a1
Simon Litchfield <simon@s29.com.au>
2023-05-04 15:47:46 +1000

    Support postgresql SSL

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+echo $DATABASE_SSLCERT > /ssl/postgresql.crt
+echo $DATABASE_SSLKEY > /ssl/postgresql.key
+echo $DATABASE_ROOTCERT > /ssl/root.crt
+
 jet_bridge --environment_type=docker --media_root=/jet/jet_media --use_default_config=project,token,address,config
